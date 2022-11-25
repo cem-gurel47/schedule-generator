e0c50e9f061e065d84d277644a3ad67bafc9aab5
Cem Gurel <gurel.cem374@hotmail.com>
2022-11-25 13:37:06 -0500

    created headers with api requests with static data

---
+++ src/components/layout/Header/GuestHeader.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+
+const GuestHeader = () => {
+  return (
+    <div className="sticky -top-1 left-0 z-10 mb-4 flex justify-center shadow-sm [transform:translateZ(0)]">
+      <header className="w-full max-w-6xl py-[0.8125em] px-0 backdrop-blur-sm">
+        <div className="navbar flex justify-between">
+          <ul className="flex items-center space-x-5">
+            <li>
+              <Link href="/">
+                <a
+                  role="button"
+                  className="text-3xl font-bold text-secondary transition duration-300 hover:text-white"
+                >
+                  Scheduler
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/pricing">
+                <a className=" font-medium">Pricing</a>
+              </Link>
+            </li>
+          </ul>
+          <ul className="flex items-center space-x-5">
+            <li>
+              <a>Contact</a>
+            </li>
+            <li>
+              <a className="">Log in</a>
+            </li>
+            <li>
+              <Link href="/dashboard/manager">
+                <a
+                  role="button"
+                  className="rounded-md bg-secondary py-3 px-6 text-white transition duration-300 hover:bg-secondary"
+                >
+                  Sign up
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default GuestHeader;
